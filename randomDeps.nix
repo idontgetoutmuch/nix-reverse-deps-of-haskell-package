4837e20b3a19fa18a3d3e4f2e0beea6146b488ed
Dominic Steinitz <dominic@steinitz.org>
2020-02-28 15:28:12 +0000

    Some improvements

--- randomDeps.nix
+++ randomDeps.nix
@@ -1,29 +1,28 @@
 let
+  pkgs = import <nixpkgs> {};
   myHaskellPackageOverlay = self: super: {
     haskellPackages = super.haskellPackages.override {
-    overrides = hself: hsuper: {
+      overrides = hself: hsuper: {
         mkDerivation = args: hsuper.mkDerivation (args // {
           doCheck = false;
           doHaddock = false;
           jailbreak = true;
         });
-        random =
-          let newRandomSrc = builtins.fetchGit {
-                url = "https://github.com/idontgetoutmuch/random.git";
-                rev = "83262ccd8a2d4b8f29b14c535b6bdf997bc7a497";
-                ref = "interface-to-performance";
-              };
-          in
-          # Since cabal2nix has a transitive dependency on random, we need to
-          # get the callCabal2nix function from the normal haskellPackages that
-          # is not being overridden.
-          (import <nixpkgs> {}).haskell.lib.dontCheck ((import <nixpkgs> {}).haskellPackages.callCabal2nix "random" newRandomSrc { });
+        random = pkgs.haskellPackages.callCabal2nix "random" (
+            self.fetchFromGitHub {
+              owner = "idontgetoutmuch";
+              repo = "random";
+              rev = "023e812545d2fac849ae64058d64590a63d2fe89";
+              sha256 = "1yfmc766jki1s0pr4hrp29ngfyxznz71vg303b7hrvin411q8w4l";
+              }) { };
       };
     };
   };
 
   nixpkgs = import <nixpkgs> { overlays = [ myHaskellPackageOverlay ]; };
+
 in
+
 # This example derivation file is assumed to be in the current directory.  "./default.nix" is
 # the current file you are looking at.
 import ./default.nix {
